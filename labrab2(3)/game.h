77b9d0adc3e120f3e1b3f80b3d33d0b9d3121290
awkvard <sergoshapo09@gmail.com>
2021-10-16 17:29:09 +0700

    Функции для game

--- labrab2(3)/game.h
+++ labrab2(3)/game.h
@@ -1,4 +1,6 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
 struct game
 {
 	char league_name[50];
@@ -6,8 +8,29 @@ struct game
 	char visitor_team[50];
 	char result[10];
 	//////////////////////////func
-
-
+	game(char* name, char* rez, char* vis, char* home)
+	{
+		strcpy(league_name,name);
+		strcpy(home_team,home);
+		strcpy(visitor_team,vis);
+		strcpy(result,rez);
+	};
+	void change_name(game a, char* name1)
+	{
+		strcpy(a.league_name, name1);
+	};
+	void change_home_team(game a, char* home_team1)
+	{
+		strcpy(a.home_team, home_team1);
+	};
+	void change_visitor_team(game a, char* visitor_team1)
+	{
+		strcpy(a.visitor_team, visitor_team1);
+	};
+	void change_result(game a, char* result1)
+	{
+		strcpy(a.result, result1);
+	};
 
 };
 
