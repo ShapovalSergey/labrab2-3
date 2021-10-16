2df023bbabd0968902984e293ed2e78d2b2657cc
awkvard <sergoshapo09@gmail.com>
2021-10-16 18:33:46 +0700

    Функции для goalkeeper, field_player

--- labrab2(3)/field_player.h
+++ labrab2(3)/field_player.h
@@ -1,4 +1,6 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
 #include "team.h"
 struct field_player:team
 {
@@ -15,7 +17,64 @@ struct field_player:team
 	int weight;
 	int height;
 	//////////////////////////func
-
+	field_player(const team& a, char* name1, int age1, int games1, int goals1, int assists1, int red1, int yellow1, char* position1, char* nation1, int weig1, int heig1) : team(a)
+	{
+		strcpy(name, name1);
+		strcpy(nation, nation1);
+		age = age1;
+		games = games1;
+		goals = goals1;
+		assists = assists1;
+		red_cards = red1;
+		yellow_cards = yellow1;
+		strcpy(position,position1);
+		weight = weig1;
+		height = heig1;
+	};
+	void change_name(field_player a, char* name1)
+	{
+		strcpy(a.name, name1);
+	};
+	void change_position(field_player a, char* position1)
+	{
+		strcpy(a.position, position1);
+	};
+	void change_nation(field_player a, char* nation1)
+	{
+		strcpy(a.nation, nation1);
+	};
+	void change_games(field_player a, int games1)
+	{
+		a.games = games1;
+	};
+	void change_goals(field_player a, int goals1)
+	{
+		a.goals = goals1;
+	};
+	void change_age(field_player a, int age1)
+	{
+		a.age = age1;
+	};
+	void change_red_cards(field_player a, int red_cards1)
+	{
+		a.red_cards = red_cards1;
+	};
+	void change_yellow_cards(field_player a, int yellow_cards1)
+	{
+		a.yellow_cards = yellow_cards1;
+	};
+	void change_weight(field_player a, int weight1)
+	{
+		a.weight = weight1;
+	};
+	void change_height(field_player a, int height1)
+	{
+		a.height = height1;
+	};
+	void change_assists(field_player a, int assists1)
+	{
+		a.assists = assists1;
+	};
 
 };
 
--- labrab2(3)/goalkeeper.h
+++ labrab2(3)/goalkeeper.h
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
 #pragma once
 #include "team.h"
 struct goalkeeper:team
@@ -15,14 +17,65 @@ struct goalkeeper:team
 	int weight;
 	int height;
 	//////////////////////////func
-	goalkeeper(const team &a ): team(a)
+	goalkeeper(const team &a, char* name1,int age1,int games1,int goals1, int assists1,int red1,int yellow1,int miss1, char* nation1, int weig1, int heig1 ): team(a)
 	{
-	
-
-
-
-
+		strcpy(name,name1);
+		strcpy(nation,nation1);
+		age = age1;
+		games = games1;
+		goals = goals1;
+		assists = assists1;
+		red_cards = red1;
+		yellow_cards = yellow1;
+		missed_balls = miss1;
+		weight = weig1;
+		height = heig1;
 	};
+	void change_name(goalkeeper a, char* name1 )
+	{
+		strcpy(a.name, name1);
+	};
+	void change_nation(goalkeeper a, char* nation1)
+	{
+		strcpy(a.nation, nation1);
+	};
+	void change_games(goalkeeper a, int games1)
+	{
+		a.games = games1;
+	};
+	void change_goals(goalkeeper a, int goals1)
+	{
+		a.goals = goals1;
+	};
+	void change_age(goalkeeper a, int age1)
+	{
+		a.age = age1;
+	};
+	void change_red_cards(goalkeeper a, int red_cards1)
+	{
+		a.red_cards = red_cards1;
+	};
+	void change_yellow_cards(goalkeeper a, int yellow_cards1)
+	{
+		a.yellow_cards = yellow_cards1;
+	};
+	void change_missed_balls(goalkeeper a, int missed_balls1)
+	{
+		a.missed_balls = missed_balls1;
+	};
+	void change_weight(goalkeeper a, int weight1)
+	{
+		a.weight = weight1;
+	};
+	void change_height(goalkeeper a, int height1)
+	{
+		a.height = height1;
+	};
+	void change_assists(goalkeeper a, int assists1)
+	{
+		a.assists = assists1;
+	};
+
 
 };
 
